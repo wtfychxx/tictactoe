24d7be43fbb39e06ef5fbf724959f21fdd3bcd59
fadhliyulyanto <youtube>
2026-05-15 11:12:48 +0700

    add: app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "@tanstack/start/config";
+
+export default defineConfig({
+  server: {
+    preset: "vercel", // Memberitahu Vinxi untuk menggunakan adapter Vercel
+  },
+});
